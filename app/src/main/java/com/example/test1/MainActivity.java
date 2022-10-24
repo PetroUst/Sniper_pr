dc6adb336cb037d498b40d8ab6feba4c2c3357c8
Vasyl808 <v0961491914@gmail.com>
2022-10-24 12:17:46 +0300

    update

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         editText_a = findViewById(R.id.editText_a);
         editText_b = findViewById(R.id.editText_b);
         textView_sum = findViewById(R.id.textView_sum);
