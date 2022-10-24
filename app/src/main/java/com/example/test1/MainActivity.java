f7da18eafe5ccc4677a5c08024a89af5ec8edd8d
Vasyl808 <v0961491914@gmail.com>
2022-10-24 12:14:50 +0300

    update

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -8,46 +8,39 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText_a; // Переменная для доступа к компоненту со значением "a"
-    EditText editText_b; // Переменная для доступа к компоненту со значением "b"
-    TextView textView_sum; // Переменная для доступа к компоненту со значением результата
-    Button buttonSum; // Переменная для доступа к компоненту кнопки
+    EditText editText_a;
+    EditText editText_b;
+    TextView textView_sum;
+    Button buttonSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Доступ к компонентам окна
         editText_a = findViewById(R.id.editText_a);
         editText_b = findViewById(R.id.editText_b);
         textView_sum = findViewById(R.id.textView_sum);
         buttonSum = findViewById(R.id.buttonSum);
     }
 
-    // МЕТОД КНОПКИ РАСЧЕТА
     public void onClick(View v) {
-        // Объявление локальных переменных
         double a, b, c;
 
-        try { // НАЧАЛО ЗАЩИЩЕННОГО БЛОКА
+        try {
 
-            // Чтение данных из компонент
             a = Double.parseDouble(editText_a.getText().toString().trim());
             b = Double.parseDouble(editText_b.getText().toString().trim());
 
-            // Основной алгоритм
             c = a + b;
 
-            // Вывод полученного значения в компонент
             textView_sum.setText(String.valueOf(c));
 
-        } catch (Exception e) { // ЧТО ДЕЛАТЬ ЕСЛИ ВОЗНИКНЕТ ОШИБКА В БЛОКЕ МЕЖДУ "TRY" И "CATCH":
+        } catch (Exception e) {
 
-            // Вывод сообщения об ошибке
             textView_sum.setText("Неверные входные данные для расчета!");
 
-        }  // КОНЕЦ ЗАЩИЩЕННОГО БЛОКА
+        }
 
     }
 
